9a4e6f119c94213c4d86940f44967c4f37873655
Layne Shepherd <layne_s@icloud.com>
2026-05-09 08:44:05 -0400

    update max rotater kata with instructions

--- max_rotater/src/lib.rs
+++ max_rotater/src/lib.rs
@@ -1,14 +1,16 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 
+fn max_rot(n: u64) -> u64 {
+    todo!()
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn basic() {
+        assert_eq!(max_rot(38458215), 85821534);
+        assert_eq!(max_rot(195881031), 988103115);
+        assert_eq!(max_rot(896219342), 962193428);
+        assert_eq!(max_rot(69418307), 94183076);
     }
 }
