aada4e2471c6a7c96fbdeb833c32e74208a52293
Layne Shepherd <layne_s@icloud.com>
2026-05-13 16:03:11 -0400

    working solution for categorize new member

--- categorize_new_member_5502c9e7b3216ec63c0001aa/src/lib.rs
+++ categorize_new_member_5502c9e7b3216ec63c0001aa/src/lib.rs
@@ -1,5 +1,17 @@
 fn _open_or_senior(data: Vec<(i32, i32)>) -> Vec<String> {
-    vec![]
+    const OPEN: &str = "Open";
+    const SENIOR: &str = "Senior";
+    const SENIOR_AGE_GE: i32 = 55;
+    const SENIOR_HANDICAP_GT: i32 = 7;
+    data.into_iter()
+        .map(|(age, handicap)| {
+            if age >= SENIOR_AGE_GE && handicap > SENIOR_HANDICAP_GT {
+                SENIOR.to_string()
+            } else {
+                OPEN.to_string()
+            }
+        })
+        .collect()
 }
 
 // Add your tests here.
@@ -9,7 +21,13 @@ mod tests {
     use super::*;
     #[test]
     fn returns_expected() {
-        assert_eq!(_open_or_senior(vec![(45, 12), (55, 21), (19, -2), (104, 20)]), vec!["Open", "Senior", "Open", "Senior"]);
-        assert_eq!(_open_or_senior(vec![(3, 12), (55, 1), (91, -2), (54, 23)]), vec!["Open", "Open", "Open", "Open"]);
+        assert_eq!(
+            _open_or_senior(vec![(45, 12), (55, 21), (19, -2), (104, 20)]),
+            vec!["Open", "Senior", "Open", "Senior"]
+        );
+        assert_eq!(
+            _open_or_senior(vec![(3, 12), (55, 1), (91, -2), (54, 23)]),
+            vec!["Open", "Open", "Open", "Open"]
+        );
     }
 }
