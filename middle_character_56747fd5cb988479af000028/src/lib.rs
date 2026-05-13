bbe17be39b01c64def8d1f568aeea0065fb89aa5
Layne Shepherd <layne_s@icloud.com>
2026-05-13 10:41:06 -0400

    setup for middle character kata

--- middle_character_56747fd5cb988479af000028/src/lib.rs
+++ middle_character_56747fd5cb988479af000028/src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+fn _get_middle(s:&str) -> &str {
+    todo!("get middle of string")
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[test]
+fn example_tests() {
+    assert_eq!(_get_middle("test"), "es");
+    assert_eq!(_get_middle("testing"), "t");
+    assert_eq!(_get_middle("middle"), "dd");
+    assert_eq!(_get_middle("A"), "A");
+    assert_eq!(_get_middle("of"), "of");
 }
