ece344c34494a09bd1c6b3ac92dc6b0821169aea
Layne Shepherd <layne_s@icloud.com>
2026-05-13 09:54:33 -0400

    solution for binary addition

---
+++ binary_addition_551f37452ff852b7bd000139/src/lib.rs
@@ -0,0 +1,25 @@
+fn _add_binary(a: u64, b: u64) -> String {
+    format!("{:b}", a + b)
+}
+
+// https://doc.rust-lang.org/book/testing.html
+
+#[cfg(test)]
+mod tests {
+    use super::_add_binary;
+
+    fn dotest(a: u64, b: u64, expected: &str) {
+        let actual = _add_binary(a, b);
+        assert_eq!(actual, expected,
+                "With a = {a}, b = {b}\nExpected \"{expected}\" but got \"{actual}\"")
+    }
+
+    #[test]
+    fn sample_tests() {
+        dotest(1,1,"10");
+        dotest(0,1,"1");
+        dotest(1,0,"1");
+        dotest(2,2,"100");
+        dotest(51,12,"111111");
+    }
+}
