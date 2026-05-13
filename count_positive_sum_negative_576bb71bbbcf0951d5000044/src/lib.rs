c57e632bb71be671c56c0b9a2658b82c4b0f5114
Layne Shepherd <layne_s@icloud.com>
2026-05-13 11:08:37 -0400

    setup for count positive sum negatives kata

---
+++ count_positive_sum_negative_576bb71bbbcf0951d5000044/src/lib.rs
@@ -0,0 +1,26 @@
+fn _count_positives_sum_negatives(input: Vec<i32>) -> Vec<i32> {
+    input
+}
+
+#[cfg(test)]
+mod tests {
+    use super::_count_positives_sum_negatives;
+
+    fn dotest(a: &[i32], expected: &[i32]) {
+        let actual = _count_positives_sum_negatives(a.to_vec());
+        assert_eq!(actual,expected,
+                "With input = {a:?}\nExpected {expected:?} but got {actual:?}")
+    }
+    #[test]
+    fn fixed_tests() {
+        dotest(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], &[10, -65]);
+        dotest(&[], &[]);
+        dotest(&[0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14], &[8, -50]);
+        dotest(&[0,1,2,3,4,5], &[5, 0]);
+        dotest(&[1,2,3,4,5], &[5, 0]);
+        dotest(&[0,-1,-2,-3,-4,-5], &[0, -15]);
+        dotest(&[-1,-2,-3,-4,-5], &[0, -15]);
+        dotest(&[0,0,0,0], &[0,0]);
+        dotest(&[0], &[0,0]);
+    }
+}
