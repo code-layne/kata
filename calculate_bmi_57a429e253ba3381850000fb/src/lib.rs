16892c558ce76824451da4484f4d30475344f7ea
Layne Shepherd <layne_s@icloud.com>
2026-05-15 16:24:42 -0400

    solution for calculate bmi

--- calculate_bmi_57a429e253ba3381850000fb/src/lib.rs
+++ calculate_bmi_57a429e253ba3381850000fb/src/lib.rs
@@ -1,5 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+fn _bmi(weight: u32, height: f32) -> &'static str {
+    match weight as f32 / height.powi(2) {
+        x if x <= 18.5 => "Underweight",
+        x if x <= 25.0 => "Normal",
+        x if x <= 30.0 => "Overweight",
+        _ => "Obese",
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +12,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn basic_tests() {
+        assert_eq!(_bmi(50, 1.80), "Underweight");
+        assert_eq!(_bmi(80, 1.80), "Normal");
+        assert_eq!(_bmi(90, 1.80), "Overweight");
+        assert_eq!(_bmi(110, 1.80), "Obese");
     }
 }
