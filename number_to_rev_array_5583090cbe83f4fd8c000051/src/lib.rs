986890a8425870aa707cd983484e9487c2c84df9
Layne Shepherd <layne_s@icloud.com>
2026-05-15 14:28:53 -0400

    solution for number to reverse array of digits

--- number_to_rev_array_5583090cbe83f4fd8c000051/src/lib.rs
+++ number_to_rev_array_5583090cbe83f4fd8c000051/src/lib.rs
@@ -1,5 +1,9 @@
 fn _digitize(n: u64) -> Vec<u8> {
-    Vec::from(n.to_string().as_bytes())
+    n.to_string()
+        .chars()
+        .rev()
+        .map(|c| c.to_digit(10).unwrap() as u8)
+        .collect::<Vec<u8>>()
 }
 #[cfg(test)]
 mod tests {
