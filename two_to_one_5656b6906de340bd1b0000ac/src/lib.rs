d960cc62828f56b8b16bc695bd8dd0705402bb03
Layne Shepherd <layne_s@icloud.com>
2026-05-15 15:55:56 -0400

    my solution and idiomatic solution

--- two_to_one_5656b6906de340bd1b0000ac/src/lib.rs
+++ two_to_one_5656b6906de340bd1b0000ac/src/lib.rs
@@ -1,7 +1,23 @@
-use std::ops::Add;
+use std::collections::{BTreeSet, HashSet};
 
 fn _longest(a1: &str, a2: &str) -> String {
-    a1.to_string().add(a2)
+    let added_str = a1.to_string() + &a2;
+    let mut uniq_sorted: Vec<char> = added_str
+        .chars()
+        .collect::<HashSet<char>>()
+        .into_iter()
+        .collect::<Vec<char>>();
+    uniq_sorted.sort();
+
+    uniq_sorted.iter().collect::<String>()
+}
+
+fn _longest2(a1: &str, a2: &str) -> String {
+    a1.chars()
+        .chain(a2.chars())
+        .collect::<BTreeSet<char>>()
+        .iter()
+        .collect()
 }
 
 #[cfg(test)]
@@ -9,16 +25,20 @@ mod tests {
     use super::*;
 
     fn testing(s1: &str, s2: &str, exp: &str) -> () {
+        let longest = _longest2(s1, s2);
         println!("s1:{:?} s2:{:?}", s1, s2);
-        println!("{:?} {:?}", _longest(s1, s2), exp);
-        println!("{}", _longest(s1, s2) == exp);
-        assert_eq!(&_longest(s1, s2), exp)
+        println!("{:?} {:?}", longest, exp);
+        println!("{}", longest == exp);
+        assert_eq!(&longest, exp)
     }
 
     #[test]
     fn basic_tests() {
         testing("aretheyhere", "yestheyarehere", "aehrsty");
-        testing("loopingisfunbutdangerous", "lessdangerousthancoding", "abcdefghilnoprstu");
-
+        testing(
+            "loopingisfunbutdangerous",
+            "lessdangerousthancoding",
+            "abcdefghilnoprstu",
+        );
     }
 }
