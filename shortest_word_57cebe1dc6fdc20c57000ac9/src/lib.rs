e121adb43405a8878685418f57b2a8aff0f7f6ec
Layne Shepherd <layne_s@icloud.com>
2026-05-15 16:26:54 -0400

    shortest word setup, no solution

---
+++ shortest_word_57cebe1dc6fdc20c57000ac9/src/lib.rs
@@ -0,0 +1,23 @@
+fn _find_short(s: &str) -> u32 {
+    s.len() as u32
+}
+#[cfg(test)]
+mod tests {
+    use super::_find_short;
+
+    fn dotest(s: &str, expected: u32) {
+        let actual = _find_short(s);
+        assert_eq!(actual, expected,
+                "With s = \"{s}\"\nExpected {expected} but got {actual}")
+    }
+
+    #[test]
+    fn sample_tests() {
+        dotest("bitcoin take over the world maybe who knows perhaps", 3);
+        dotest("turns out random test cases are easier than writing out basic ones", 3);
+        dotest("lets talk about javascript the best language", 3);
+        dotest("i want to travel the world writing code one day", 1);
+        dotest("Lets all go on holiday somewhere very cold", 2);
+        dotest("Let's travel abroad shall we", 2);
+    }
+}
