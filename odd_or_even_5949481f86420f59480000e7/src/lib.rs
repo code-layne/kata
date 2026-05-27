173a39043cfe4305928fa865c62680623dfd8562
Layne Shepherd <layne_s@icloud.com>
2026-05-27 17:35:08 -0400

    solution for odd or even

---
+++ odd_or_even_5949481f86420f59480000e7/src/lib.rs
@@ -0,0 +1,40 @@
+fn _odd_or_even(numbers: Vec<i32>) -> String {
+    match numbers.iter().sum::<i32>() % 2 {
+        0 => "even",
+        _ => "odd",
+    }.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_empty_array() {
+        assert_eq!(_odd_or_even(vec![]), "even");
+    }
+    #[test]
+    fn test_single_even() {
+        assert_eq!(_odd_or_even(vec![0]), "even");
+    }
+    #[test]
+    fn test_single_odd() {
+        assert_eq!(_odd_or_even(vec![1]), "odd");
+    }
+    #[test]
+    fn test_even() {
+        assert_eq!(_odd_or_even(vec![0, 1, 5]), "even");
+    }
+    #[test]
+    fn test_odd() {
+        assert_eq!(_odd_or_even(vec![0, 1, 4]), "odd");
+    }
+    #[test]
+    fn test_negative_even() {
+        assert_eq!(_odd_or_even(vec![0, -1, -5]), "even");
+    }
+    #[test]
+    fn test_negative_odd() {
+        assert_eq!(_odd_or_even(vec![0, -1, 2]), "odd");
+    }
+}
