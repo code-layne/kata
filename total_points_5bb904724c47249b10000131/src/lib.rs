1b34b90876427b0e2ba3a2af06ce429ef153ed25
Layne Shepherd <layne_s@icloud.com>
2026-05-29 09:28:22 -0400

    setup for total points, no solution

---
+++ total_points_5bb904724c47249b10000131/src/lib.rs
@@ -0,0 +1,26 @@
+fn _points(games: &[String]) -> u32 {
+    games.len() as u32
+}
+
+#[cfg(test)]
+mod tests {
+    use super::_points;
+
+    const ERR_MSG: &str = "\nYour result (left) did not match the expected output (right)";
+
+    fn do_fixed_test(e: &[&str], expected: u32) {
+        let a = &e.iter().map(|x| x.to_string()).collect::<Vec<_>>();
+        assert_eq!(_points(a), expected, "{ERR_MSG} with games = {a:?}")
+    }
+
+    #[test]
+    fn fixed_tests() {
+        do_fixed_test(&["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"], 30);
+        do_fixed_test(&["1:1", "2:2", "3:3", "4:4", "2:2", "3:3", "4:4", "3:3", "4:4", "4:4"], 10);
+        do_fixed_test(&["0:1", "0:2", "0:3", "0:4", "1:2", "1:3", "1:4", "2:3", "2:4", "3:4"], 0);
+        do_fixed_test(&["1:0", "2:0", "3:0", "4:0", "2:1", "1:3", "1:4", "2:3", "2:4", "3:4"], 15);
+        do_fixed_test(&["1:0", "2:0", "3:0", "4:4", "2:2", "3:3", "1:4", "2:3", "2:4", "3:4"], 12);
+    }
+}
+
+
